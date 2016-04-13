0a2b446d7176ef255a46f6125bca285cc35c12e8
trival <thomas.gorny@mail.com>
2016-04-13 17:02:52 +0200

    implement basic add entity modal

--- src/cljs/flow_editor/views/modals/add_entity.cljs
+++ src/cljs/flow_editor/views/modals/add_entity.cljs
@@ -1,5 +1,30 @@
-(ns flow-editor.views.modals.add-entity)
+(ns flow-editor.views.modals.add-entity
+  (:require [reagent.core :as r]
+            [re-frame.core :refer [dispatch]]
+            [re-com.core :refer [title button v-box h-box modal-panel input-text line]]))
 
 
 (defn add-entity-modal []
-  [:div {:class-name "add-entity-modal"} "add-entity-modal"])
+  (let [entity-id (r/atom "")]
+    (fn []
+      [modal-panel
+       :child [v-box
+               :children [[title
+                           :label "Enter entity id"
+                           :level :level2
+                           :margin-bottom "20px"]
+                          [input-text
+                           :model @entity-id
+                           :on-change #(reset! entity-id %)
+                           :placeholder "Entity id"]
+                          [line
+                           :color "#ddd" :style {:margin "20px"}]
+                          [h-box
+                           :gap "12px"
+                           :children [[button
+                                       :label "Create"
+                                       :class "btn-primary"
+                                       :on-click #(dispatch [:runtime-update/add-entity @entity-id])]
+                                      [button
+                                       :label "Cancel"
+                                       :on-click #(dispatch [:modals/close])]]]]]])))
